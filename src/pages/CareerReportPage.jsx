549ae4657ef6cf73f8b1a491fa7c44b2df89be07
Yagya Deo Sharma <deoyagya@gmail.com>
2026-03-23 15:47:45 +1100

    feat: route career report through product landing

--- src/pages/CareerReportPage.jsx
+++ src/pages/CareerReportPage.jsx
@@ -1,11 +1,5 @@
-import PageShell from '../components/PageShell';
-import ReportLandingPage from '../components/ReportLandingPage';
-import { careerConfig } from '../data/reportLandingConfigs';
+import ProductReportLandingPage from './ProductReportLandingPage';
 
 export default function CareerReportPage() {
-  return (
-    <PageShell activeNav="reports">
-      <ReportLandingPage config={careerConfig} />
-    </PageShell>
-  );
+  return <ProductReportLandingPage slug="career" defaultActiveNav="kundli" />;
 }
